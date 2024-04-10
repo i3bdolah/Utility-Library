9b8753c49573538b266b692ab0942a90224fd80e
i3bdolah <xbdullahx@gmail.com>
2024-04-11 01:23:49 +0300

    Adding PrintNumberTextName Function

--- UtilityLibrary/Utility.h
+++ UtilityLibrary/Utility.h
@@ -397,4 +397,55 @@ public:
 	static void Swap(Date& value1, Date& value2) {
 		Date::SwapTwoDates(value1, value2);
 	}
+
+	string PrintNumberTextName(int num) {
+		if (num == 0)
+		{
+			return "";
+		}
+		if (num >= 1 && num <= 19)
+		{
+			string arr[] = { "", "One","Two","Three","Four","Five","Six","Seven", "Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen", "Fifteen","Sixteen","Seventeen","Eighteen","Nineteen" };
+			return arr[num] + " ";
+		}
+		if (num >= 20 && num <= 99)
+		{
+			string arr[] = { "","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety" };
+			return arr[num] + " " + PrintNumberTextName(num % 10);
+		}
+		if (num >= 100 && num <= 199)
+		{
+			return "One Hundred " + PrintNumberTextName(num % 100);
+		}
+		if (num >= 200 && num <= 999)
+		{
+			return PrintNumberTextName(num / 100) + "Hundreds " + PrintNumberTextName(num % 100);
+		}
+		if (num >= 1000 && num <= 1999)
+		{
+			return "One Thousand " + PrintNumberTextName(num % 1000);
+		}
+		if (num >= 2000 && num <= 999999)
+		{
+			return PrintNumberTextName(num / 1000) + "Thousands " + PrintNumberTextName(num % 1000);
+		}
+		if (num >= 1000000 && num <= 1999999)
+		{
+			return "One Million " + PrintNumberTextName(num % 1000000);
+		}
+		if (num >= 2000000 && num <= 999999999)
+		{
+			return PrintNumberTextName(num / 1000000) + "Millions " + PrintNumberTextName(num % 1000000);
+		}
+		if (num >= 1000000000 && num <= 1999999999)
+		{
+			return "One Billion " + PrintNumberTextName(num % 1000000000);
+		}
+		else
+		{
+			return PrintNumberTextName(num / 1000000000) + "Billions " + PrintNumberTextName(num % 1000000000);
+
+		}
+	}
+
 };
